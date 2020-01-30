e2bf1be70a6263f68108df72a8a353d71041fee3
Lucas Adorno <lucasadorno16@hotmail.com>
2020-01-30 07:38:58 -0300

    Adicionado possibilidade de porta customizado para o banco no container docker

--- src/config/database.js
+++ src/config/database.js
@@ -7,6 +7,7 @@ module.exports = {
   username: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE_NAME,
+  port: process.env.DATABASE_PORT,
   define: {
     timestamps: true,
     underscored: true,
