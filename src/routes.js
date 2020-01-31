543b3246ba57433e2ccf963eae593da3ef7e92e3
Lucas Adorno <lucasadorno16@hotmail.com>
2020-01-31 10:02:51 -0300

    Adiciona lógica de Provider no projeto

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import multer from 'multer';
 import FileController from './app/controllers/FileController';
+import ProviderController from './app/controllers/ProviderController';
 import SessionController from './app/controllers/SessionController';
 import UserController from './app/controllers/UserController';
 import authMiddleware from './app/middlewares/auth';
@@ -20,4 +21,6 @@ routes.post(
   upload.single('file'),
   FileController.store
 );
+
+routes.get('/providers', authMiddleware, ProviderController.index);
 export default routes;
