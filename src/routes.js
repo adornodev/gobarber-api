acd249ba47214c1dda39e687d6573fb462b566b6
Lucas Adorno <lucasadorno16@hotmail.com>
2020-01-31 09:39:11 -0300

    Adiciona dados referentes ao FileController

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,23 @@
 import { Router } from 'express';
+import multer from 'multer';
+import FileController from './app/controllers/FileController';
 import SessionController from './app/controllers/SessionController';
 import UserController from './app/controllers/UserController';
 import authMiddleware from './app/middlewares/auth';
+import multerConfig from './config/multer';
 
 const routes = Router();
+const upload = multer(multerConfig);
 
 routes.post('/users', UserController.store);
-routes.post('/sessions', SessionController.store);
 routes.put('/users', authMiddleware, UserController.update);
 
+routes.post('/sessions', SessionController.store);
+
+routes.post(
+  '/files',
+  authMiddleware,
+  upload.single('file'),
+  FileController.store
+);
 export default routes;
