113e21c74e3a3879877eb688efacd7457b861f8d
Lucas Adorno <lucasadorno16@hotmail.com>
2020-02-01 19:49:11 -0300

    Adicionado dotenv nos arquivos

--- src/config/database.js
+++ src/config/database.js
@@ -3,11 +3,11 @@ require('dotenv').config();
 
 module.exports = {
   dialect: 'postgres',
-  host: process.env.DATABASE_HOST,
-  username: process.env.DATABASE_USER,
-  password: process.env.DATABASE_PASSWORD,
-  database: process.env.DATABASE_NAME,
-  port: process.env.DATABASE_PORT,
+  host: process.env.MAIN_DATABASE_HOST,
+  username: process.env.MAIN_DATABASE_USER,
+  password: process.env.MAIN_DATABASE_PASSWORD,
+  database: process.env.MAIN_DATABASE_NAME,
+  port: process.env.MAIN_DATABASE_PORT,
   define: {
     timestamps: true,
     underscored: true,
