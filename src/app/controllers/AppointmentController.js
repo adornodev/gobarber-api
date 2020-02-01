3d455de4c9262f5ab9e0349eeb132cdc08843f5b
Lucas Adorno <lucasadorno16@hotmail.com>
2020-02-01 08:41:32 -0300

    Adicionado estilização do envio de email

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -133,6 +133,7 @@ class AppointmentController {
           as: 'provider',
           attributes: ['name', 'email'],
         },
+        { model: User, as: 'user', attributes: ['name'] },
       ],
     });
 
@@ -163,11 +164,14 @@ class AppointmentController {
     await Mail.sendMail({
       to: `${appointment.provider.name} <${appointment.provider.email}>`,
       subject: 'Agendamento Cancelado',
-      text: `Você tem um novo cancelamento! Executado cancelamento da data ${format(
-        appointment.date,
-        'dd/MM/yyyy H:mm:ss',
-        { locale: pt }
-      )}`,
+      template: 'cancellation',
+      context: {
+        provider: appointment.provider.name,
+        user: appointment.user.name,
+        date: format(appointment.date, "'dia' dd 'de' MMMM', às' H:mm'h'", {
+          locale: pt,
+        }),
+      },
     });
 
     return res.json(appointment);
