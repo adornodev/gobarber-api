771e6efc7f4a2578519da6a9f764e618359b8619
Lucas Adorno <lucasadorno16@hotmail.com>
2020-02-01 19:37:37 -0300

    Adiciona tratativa de erros

--- src/app.js
+++ src/app.js
@@ -1,17 +1,25 @@
+import * as Sentry from '@sentry/node';
 import cors from 'cors';
 import express from 'express';
+import 'express-async-errors';
 import path from 'path';
+import Youch from 'youch';
+import sentryConfig from './config/sentry';
 import './database';
 import routes from './routes';
 
 class App {
   constructor() {
     this.server = express();
+
+    Sentry.init(sentryConfig);
     this.middleware();
     this.routes();
+    this.exceptionHandler();
   }
 
   middleware() {
+    this.server.use(Sentry.Handlers.requestHandler());
     this.server.use(cors());
     this.server.use(express.json());
 
@@ -24,6 +32,25 @@ class App {
 
   routes() {
     this.server.use(routes);
+    this.server.use(
+      Sentry.Handlers.errorHandler({
+        shouldHandleError(error) {
+          // Capture all 404 and 500 errors
+          if (error.status === 404 || error.status === 500) {
+            return true;
+          }
+          return false;
+        },
+      })
+    );
+  }
+
+  exceptionHandler() {
+    this.server.use(async (err, req, res, next) => {
+      const errors = await new Youch(err, req).toJSON();
+
+      return res.status(500).json(errors);
+    });
   }
 }
 
---
+++ src/config/sentry.js
@@ -0,0 +1,3 @@
+export default {
+  dsn: 'https://ee2b2c5aa66c42ff8913d5e5a17f18f5@sentry.io/2162949',
+};
